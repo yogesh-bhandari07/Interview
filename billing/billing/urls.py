f28db9144afc1f3c1bbde510a3077113b453706f
Yogesh Bhandari <yogesh.bhandari285@gmail.com>
2024-04-13 10:37:44 +0530

    redirection

--- billing/billing/urls.py
+++ billing/billing/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from management.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('management.urls')),
+    path('',home),
 ]
